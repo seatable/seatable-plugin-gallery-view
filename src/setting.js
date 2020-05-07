c703da9e16b4c6f4c42c0e18aea9cac10659498f
zxj96 <519213124@qq.com>
2020-05-07 09:40:35 +0800

    setting

--- src/setting.js
+++ src/setting.js
@@ -1,9 +1,9 @@
 
 const config = {
-  APIToken: "dff4d16acdfe3042c25e5e52ea60c9831c79bf02",
+  APIToken: "",
   server: "https://dev.seafile.com/dtable-web",
-  workspaceID: "4",
-  dtableName: "test2",
+  workspaceID: "",
+  dtableName: "",
   lang: "en"
 };
 
