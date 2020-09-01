4ebe74a30b910e0c10978dcaf8c74cc9ae6680c0
zxj96 <519213124@qq.com>
2020-09-01 13:49:09 +0800

    setting.js

---
+++ src/setting.js
@@ -0,0 +1,11 @@
+
+const config = {
+  APIToken: "",
+  server: "https://dev.seafile.com/dtable-web",
+  workspaceID: "",
+  dtableName: "",
+  lang: "en"
+};
+
+const dtablePluginConfig = Object.assign({}, config, {server: config.server.replace(/\/+$/, "")});
+window.dtablePluginConfig = dtablePluginConfig; 
