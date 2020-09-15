5bfd5317d1cd8e3d259e295acc9ad6d06ba81f14
shanshuirenjia <978987373@qq.com>
2020-09-15 15:03:56 +0800

    update translate script

--- scripts/translate.js
+++ scripts/translate.js
@@ -4,23 +4,23 @@ const fs = require('fs');
 const sourcePath = path.resolve(__dirname, '../public/locale');
 const targetPath = path.resolve(__dirname, '../src/locale/lang');
 
-const SUPPORT_LANGUAGES = ['de', 'en', 'fr', 'zh_CN'];
+const SUPPORT_LANGUAGES = ['en', 'de', 'fr', 'zh_CN'];
 const files = fs.readdirSync(path.join(sourcePath, SUPPORT_LANGUAGES[0]));
 const pluginName = files[0];
 
 const generatorLanguage = (lang, content) => {
   return (
-`
-const ${lang} = ${content};
+`const ${lang} = ${content};
 
-export default ${lang};
-`
+export default ${lang};`
   );
 }
 
 SUPPORT_LANGUAGES.forEach(lang => {
   // 读出
   const contentPath = path.join(sourcePath, lang, pluginName);
+  const isExist = fs.existsSync(contentPath);
+  if (!isExist) return;
   const content = fs.readFileSync(contentPath, 'utf-8');
 
   // 写入
