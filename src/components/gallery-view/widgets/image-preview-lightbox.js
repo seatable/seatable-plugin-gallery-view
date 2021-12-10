d353c5b177fbfc3c3a49527aa9cde78fc89a2d5c
Michael An <1822852997@qq.com>
2021-12-10 15:32:37 +0800

    change image preview light box style

--- src/components/gallery-view/widgets/image-preview-lightbox.js
+++ src/components/gallery-view/widgets/image-preview-lightbox.js
@@ -1,82 +1,132 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'reactstrap';
 import Lightbox from '@seafile/react-image-lightbox';
 import * as zIndexes from '../../../constants/zIndexes';
-import { checkDesktop } from '../../../utils/utils';
+import { checkSVGImage, isInternalImg, getImageThumbnailUrl, needUseThumbnailImage } from '../../../utils/utils';
 import ModalPortal from '../../../components/dialog/modal-portal';
 
 import '@seafile/react-image-lightbox/style.css';
+import '../../../assets/css/image-previewer-ligntbox.css';
 
 function ImagePreviewerLightbox(props) {
-  const { imageItems, imageIndex } = props;
+  const {
+    imageItems,
+    imageIndex,
+    deleteImage,
+    downloadImage,
+    onRotateImage,
+    moveToPrevRowImage,
+    moveToNextRowImage,
+    readOnly
+  } = props;
   const imageItemsLength = imageItems.length;
-  const imageURL = imageItems[imageIndex];
-
-  const sliceStartIndex = imageURL.lastIndexOf('/') + 1;
-  const sliceEndIndex = imageURL.indexOf('?') == -1 ? undefined : imageURL.indexOf('?');
-  const imageTitle = imageURL ?
-    decodeURIComponent(imageURL.slice(sliceStartIndex, sliceEndIndex)) : '';
-
-  const isDesktop = checkDesktop();
-  let overlay = {
-    zIndex: zIndexes.IMAGE_PREVIEW_LIGHTBOX
-  };
-  let reactModalStyle = { overlay };
-  if (!isDesktop) {
-    Object.assign(overlay, {
-      backgroundColor: '#000'
-    });
+  const URL = imageItems[imageIndex];
+  
+  // Handle URL has special symbol %$
+  let imageTitle = '';
+  try {
+    imageTitle = URL ? decodeURI(URL.slice(URL.lastIndexOf('/') + 1)) : '';
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
   }
+  // svg image is vectorgraph and can't rotate, external image can't rotate
+  const canRotateImage = onRotateImage && !readOnly && !checkSVGImage(URL) && isInternalImg(URL);
 
-  const downloadImage = () => {
-    let image = new Image();
-    image.crossOrigin = 'anonymous';
-    image.onload = function () {
-      let canvas = document.createElement('canvas');
-      canvas.width = this.naturalWidth;
-      canvas.height = this.naturalHeight;
-      canvas.getContext('2d').drawImage(this, 0, 0);
-
-      let eleA = document.createElement('a');
-      eleA.href = canvas.toDataURL('image/png');
-      eleA.download = imageTitle;
-      eleA.click();
-    };
-    image.src = imageURL;
-  };
-
+  let mainSrc = URL;
+  if (needUseThumbnailImage(URL)) {
+    mainSrc = getImageThumbnailUrl(URL, 512)
+  }
   const imageTitleEl = (
     <span className="d-flex">
       <span className="text-truncate">{imageTitle}</span>
       <span className="flex-shrink-0">({imageIndex + 1}/{imageItemsLength})</span>
     </span>
   );
-
-  return (
-    <Fragment>
-      <Lightbox
-        imageTitle={imageTitleEl}
-        mainSrc={imageItems[imageIndex]}
-        nextSrc={imageItems[(imageIndex + 1) % imageItemsLength]}
-        prevSrc={imageItems[(imageIndex + imageItemsLength - 1) % imageItemsLength]}
-        onCloseRequest={props.closeImagePopup}
-        onMovePrevRequest={props.moveToPrevImage}
-        onMoveNextRequest={props.moveToNextImage}
-        animationDisabled={true}
-        reactModalStyle={reactModalStyle}
-        enableZoom={isDesktop}
-        isDesktop={isDesktop}
-      />
-      {!isDesktop && (
+  if (window.isMobile) {
+    return (
+      <Fragment>
+        <Lightbox
+          isDesktop={false}
+          wrapperClassName="mobile-image-previewer"
+          mainSrc={mainSrc}
+          nextSrc={imageItems[(imageIndex + 1) % imageItemsLength]}
+          prevSrc={imageItems[(imageIndex + imageItemsLength - 1) % imageItemsLength]}
+          onCloseRequest={props.closeImagePopup}
+          onMovePrevRequest={props.moveToPrevImage}
+          onMoveNextRequest={props.moveToNextImage}
+          imagePadding={0}
+          imageTitle={imageTitleEl}
+          reactModalStyle={{
+            overlay: {
+              zIndex: zIndexes.IMAGE_PREVIEW_LIGHTBOX,
+              backgroundColor: '#000',
+            }
+          }}
+        />
         <ModalPortal>
-          <Button className="text-white position-fixed" style={{
-            zIndex: zIndexes.IMAGE_PREVIEW_LIGHTBOX + 1,
-            background: '#333', bottom: '20px', right:'20px'
-          }} onClick={downloadImage}><span className="dtable-font dtable-icon-download"></span></Button>
+          <div className="image-footer-choice mobile-image-footer-choice">
+            <div className="image-footer-icon">
+              <div className="d-flex">
+                {canRotateImage && 
+                  <span className="image-footer-choice-item mr-4" onClick={() => {onRotateImage(imageIndex, 90);}}>
+                    <i className="dtable-font dtable-icon-rotate"></i>
+                  </span>
+                }
+                {downloadImage && (
+                  <span className="image-footer-choice-item" onClick={() => {downloadImage(URL);}}>
+                    <i className="dtable-font dtable-icon-download"></i>
+                  </span>
+                )}
+              </div>
+              {(!readOnly && deleteImage) &&
+                <span className="image-footer-choice-item" onClick={() => {deleteImage(imageIndex, 'previewer');}}>
+                  <i className="dtable-font dtable-icon-delete"></i>
+                </span>
+              }
+            </div>
+          </div>
         </ModalPortal>
-      )}
-    </Fragment>
+      </Fragment>
+    );
+  }
+
+  let PCtoolbarButtons = [];
+  if (moveToPrevRowImage) {
+    PCtoolbarButtons.push(
+      <button className='dtable-font dtable-icon-retract' onClick={() => {moveToPrevRowImage();}}></button>
+    );
+  }
+  if (moveToNextRowImage) {
+    PCtoolbarButtons.push(
+      <button className='dtable-font dtable-icon-display' onClick={() => {moveToNextRowImage();}}></button>
+    );
+  }
+  if (!readOnly && deleteImage) {
+    PCtoolbarButtons.push(<button className='dtable-font dtable-icon-delete' onClick={() => {deleteImage(imageIndex, 'previewer');}}></button>);
+  }
+  if (downloadImage) {
+    PCtoolbarButtons.push(<button className='dtable-font dtable-icon-download' onClick={() => {downloadImage(URL);}}></button>); 
+  }
+  return (
+    <Lightbox
+      wrapperClassName="PC-image-previewer"
+      imageTitle={imageTitleEl}
+      toolbarButtons={PCtoolbarButtons}
+      mainSrc={mainSrc}
+      nextSrc={imageItems[(imageIndex + 1) % imageItemsLength]}
+      prevSrc={imageItems[(imageIndex + imageItemsLength - 1) % imageItemsLength]}
+      onCloseRequest={props.closeImagePopup}
+      onMovePrevRequest={props.moveToPrevImage}
+      onMoveNextRequest={props.moveToNextImage}
+      onRotateImage={canRotateImage ? (deg) => {onRotateImage(imageIndex, deg);} : null}
+      imagePadding={70}
+      reactModalStyle={{
+        overlay: {
+          zIndex: zIndexes.IMAGE_PREVIEW_LIGHTBOX
+        }
+      }}
+    />
   );
 }
 
@@ -86,6 +136,12 @@ ImagePreviewerLightbox.propTypes = {
   closeImagePopup: PropTypes.func.isRequired,
   moveToPrevImage: PropTypes.func.isRequired,
   moveToNextImage: PropTypes.func.isRequired,
+  moveToPrevRowImage: PropTypes.func,
+  moveToNextRowImage: PropTypes.func,
+  downloadImage: PropTypes.func,
+  deleteImage: PropTypes.func,
+  onRotateImage: PropTypes.func,
+  readOnly: PropTypes.bool,
 };
 
 export default ImagePreviewerLightbox;
